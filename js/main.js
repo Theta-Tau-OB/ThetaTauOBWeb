ae5b4be88bf281501aa9112b1cc40da34bee1483
Allen Polak <PolakAllen@gmail.com>
2014-03-15 17:09:11 -0400

    Total rewrite. Simplified html, removed unused

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function load(el,target) {
+    $(el).html('Hello ' + target)
+}
+var paths = {
+    '/':{'fn':load,'arg':'content/about.html'},
+    'about':{'fn':load, 'arg':'content/about.html'},
+    'contact':{'fn':load, 'arg':'content/contact.html'},
+    'events':{'fn':load, 'arg':'content/events.html'},
+    'members':{'fn':loadMembers, 'arg':'content/members.json'}
+}
+
+function router() {
+    var url = location.hash.slice(1) || '/';
+    paths[url].fn('#content',url,paths[url].arg)
+}
